5c6b5703eb5c746ccabef3a44950c42bb331483c
yonghyeun <ttddcc119@naver.com>
2025-01-30 01:22:17 +0900

    fix : textArea 높이 자동 조절 하도록 기능 수정

--- app/feature/snippet/snippetInput.tsx
+++ app/feature/snippet/snippetInput.tsx
@@ -1,24 +1,35 @@
-import { useAppDispatch, useAppSelector } from '@/hooks/redux-hooks';
-import { changeText } from './snippetSlice';
-
-import styles from './snippet.module.css';
+import { useAppDispatch, useAppSelector } from "@/hooks/redux-hooks";
+import { changeText } from "./snippetSlice";
+import styles from "./snippet.module.css";
+import { useEffect, useRef } from "react";
 
 const SnippetInput = () => {
   const { text: snippetInput } = useAppSelector((state) => state.snippet);
   const dispatch = useAppDispatch();
+  const textArea = useRef<HTMLTextAreaElement>(null);
 
   const handleCodeInput = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
     dispatch(changeText(event.target.value));
   };
 
+  useEffect(() => {
+    if (!textArea.current) {
+      return;
+    }
+
+    textArea.current.style.height = "auto";
+    textArea.current.style.height = `${textArea.current.scrollHeight}px`;
+  }, [snippetInput]);
+
   return (
     <>
-      <label htmlFor='snippetInput'></label>
+      <label htmlFor="snippetInput"></label>
       <textarea
+        ref={textArea}
         className={styles.snippetInput}
         onChange={handleCodeInput}
         defaultValue={snippetInput}
-        id='sinippetInput'
+        id="sinippetInput"
       />
     </>
   );
