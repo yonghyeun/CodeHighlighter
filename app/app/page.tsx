b99f4a15c58c33b983d61e46fe3d200ebef7ee31
yonghyeun <ttddcc119@naver.com>
2025-02-02 23:46:55 +0900

    refactor[#6] : Page 에서만 사용되는 컴포넌트 Page 파일내에서 선언하여 불필요한 파일 개수 제거

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,9 +1,6 @@
 "use client";
 
 import SnippetInput from "@/feature/snippet/snippetInput";
-import Logo from "@/components/Logo";
-import Introduction from "@/components/Introduce";
-
 import SettingBox from "@/feature/setting/settingBox";
 import SnippetOutput from "@/feature/snippet/snippetOutput";
 
@@ -20,7 +17,7 @@ export default function Home() {
         <section className="flex flex-col flex-1 bg-slate-900 md:bg-black-100 overflow-x-scroll output ">
           <div className="flex flex-col items-center  h-full gap-5  overflow-scroll output">
             <h1 className="inline">
-              <Logo />
+              <CodeHighlighterLogo />
             </h1>
             <section className="flex flex-col items-center w-full">
               <div className=" flex w-[90%] px-[1.5rem] mb-2 gap-[0.5rem] justify-end">
@@ -28,7 +25,7 @@ export default function Home() {
                 <DownLoadSnippetToSVGButton />
               </div>
               <SnippetOutput />
-              <Introduction />
+              <IntroduceText />
             </section>
           </div>
         </section>
@@ -42,3 +39,90 @@ export default function Home() {
     </main>
   );
 }
+
+const IntroduceText = () => {
+  return (
+    <div className="container md:hidden mx-auto px-4 py-8 w-[90%] min-w-[390px]">
+      <h2 className="text-2xl font-semibold mb-4">Key Features</h2>
+      <ul className="list-disc list-inside space-y-4">
+        <li>
+          <strong>Code Language Selection:</strong> Choose from a variety of
+          programming languages to highlight your code accurately.
+        </li>
+        <li>
+          <strong>Theme Setting:</strong> Personalize the appearance of your
+          code with multiple theme options.
+        </li>
+        <li>
+          <strong>Code Start Number Setting:</strong> Define the starting line
+          number for your code to provide better context.
+        </li>
+        <li>
+          <strong>Code Highlighting Options:</strong> Highlight your code with
+          three distinct styles:
+          <ul className="list-disc list-inside mt-2 space-y-2 pl-4">
+            <li>
+              <strong>Add:</strong> Highlight added lines with a specific color.
+            </li>
+            <li>
+              <strong>Remove:</strong> Highlight removed lines with a different
+              color.
+            </li>
+            <li>
+              <strong>Pointing:</strong> Highlight specific lines that need
+              attention.
+            </li>
+          </ul>
+        </li>
+        <li>
+          <strong>Color Setting and Line Setting:</strong> Customize the colors
+          used for highlighting and specify which lines should be highlighted.
+        </li>
+      </ul>
+      <p className="text-lg  mt-6">
+        You can view the entire code on{" "}
+        <a
+          href="https://github.com/yonghyeun/CodeHighlighter"
+          className="text-blue-500 "
+        >
+          GitHub
+        </a>
+        .
+      </p>
+    </div>
+  );
+};
+
+const CodeHighlighterLogo = () => (
+  <svg width="400" height="100" xmlns="http://www.w3.org/2000/svg">
+    <rect width="100%" height="100%" fill="transparent" />
+    <text
+      x="50%"
+      y="50%"
+      dominantBaseline="middle"
+      textAnchor="middle"
+      fontFamily="body-font, sans-serif"
+      fontSize="45"
+      fontWeight="700"
+      fontStyle="italic"
+      fill="#61dafb"
+      style={{ filter: "drop-shadow(2px 2px 0px #000000)" }}
+    >
+      HighlightCode
+    </text>
+    <text
+      x="50%"
+      y="50%"
+      dominantBaseline="middle"
+      textAnchor="middle"
+      fontFamily="body-font, sans-serif"
+      fontSize="45"
+      fontWeight="700"
+      fontStyle="italic"
+      fill="#61dafb"
+      style={{ filter: "drop-shadow(1px 1px 0px #000000)" }}
+    >
+      HighlightCode
+    </text>
+  </svg>
+);
