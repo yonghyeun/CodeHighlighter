3804b355a7081f6a9d9aea87c6f1f1539e811048
yonghyeun <ttddcc119@naver.com>
2025-02-04 16:55:28 +0900

    feaet[#13] : 로딩 애니메이션 추가

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -1,27 +1,27 @@
-import './globals.css';
-import { Analytics } from '@vercel/analytics/react';
-import type { Metadata } from 'next';
+import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
+import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: 'HighlightCode - Convert Text Code to Highlighted Images',
+  title: "HighlightCode - Convert Text Code to Highlighted Images",
   description:
-    'Code Highlighter transforms text code into visually appealing images with highlighted lines, perfect for sharing and presenting code snippets.',
+    "Code Highlighter transforms text code into visually appealing images with highlighted lines, perfect for sharing and presenting code snippets.",
   keywords:
-    'code highlighter, code to image, highlighted code, code snippets, code presentation, code sharing',
-  robots: 'index,follow',
+    "code highlighter, code to image, highlighted code, code snippets, code presentation, code sharing",
+  robots: "index,follow",
   openGraph: {
-    title: 'HighlightCode',
+    title: "HighlightCode",
     description:
-      'Code Highlighter transforms text code into visually appealing images with highlighted lines, perfect for sharing and presenting code snippets.',
-    images: '/asset/thumbnail.png',
-    url: 'highlightcode.site',
-    type: 'website',
+      "Code Highlighter transforms text code into visually appealing images with highlighted lines, perfect for sharing and presenting code snippets.",
+    images: "/asset/thumbnail.png",
+    url: "highlightcode.site",
+    type: "website",
   },
   twitter: {
-    title: 'HighlightCode',
+    title: "HighlightCode",
     description:
-      'Code Highlighter transforms text code into visually appealing images with highlighted lines, perfect for sharing and presenting code snippets.',
-    images: '/asset/thumbnail.png',
+      "Code Highlighter transforms text code into visually appealing images with highlighted lines, perfect for sharing and presenting code snippets.",
+    images: "/asset/thumbnail.png",
   },
 };
 
@@ -31,15 +31,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
-      <link rel='icon' href='asset/favicon.ico'></link>
+    <html lang="en">
+      <link rel="icon" href="asset/favicon.ico"></link>
       <meta
-        name='google-site-verification'
-        content='5dPXAxRRopl2GMdIlSt-fb7P9YcSH7oCGjGaHmpjP6U'
+        name="google-site-verification"
+        content="5dPXAxRRopl2GMdIlSt-fb7P9YcSH7oCGjGaHmpjP6U"
       />
       <meta
-        name='google-site-verification'
-        content='F_4Jl1HCjr89dFUxZXEiET0Cs4XSkLb3FdPLJdxu93o'
+        name="google-site-verification"
+        content="F_4Jl1HCjr89dFUxZXEiET0Cs4XSkLb3FdPLJdxu93o"
       />
       <Analytics />
       <body>{children}</body>
