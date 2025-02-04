6f8681838d47289d69685eea1a56a1b0d3b9c9d6
yonghyeun <ttddcc119@naver.com>
2025-02-05 01:35:00 +0900

    feat[#15] : withTooltip memoized 추가

--- app/src/shared/lib/withTooltip.tsx
+++ app/src/shared/lib/withTooltip.tsx
@@ -1,4 +1,4 @@
-import React, { ComponentType, PropsWithChildren, useState } from "react";
+import React, { ComponentType, PropsWithChildren } from "react";
 import tooltipStyles from "./styles.module.css";
 
 interface TooltipProps {
@@ -20,7 +20,13 @@ export const withTooltip =
   <P extends object>(Component: ComponentType<P>) =>
   (tooltipText: string, direction: 1 | 0) => {
     const WrappedComponent: React.FC<P> = (props: P) => {
-      const [isMouseEnter, setIsMouseEnter] = useState<boolean>(false);
+      // memoization 은 선택적으로 사용 하도 된다.
+      const MemoizedComponent = React.useMemo(
+        () => <Component {...props} />,
+        [props]
+      );
+
+      const [isMouseEnter, setIsMouseEnter] = React.useState<boolean>(false);
 
       const handleMouseEnter = () => setIsMouseEnter(true);
       const handleMouseLeave = () => setIsMouseEnter(false);
@@ -34,7 +40,7 @@ export const withTooltip =
           {isMouseEnter && (
             <Tooltip direction={direction}>{tooltipText}</Tooltip>
           )}
-          <Component {...props} />
+          {MemoizedComponent}
         </div>
       );
     };
