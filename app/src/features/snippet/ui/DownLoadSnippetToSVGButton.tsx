83c85fa286c6137ff749b15b2abe90ffca575c5c
yonghyeun <ttddcc119@naver.com>
2025-02-04 16:41:09 +0900

    feat[#13] : 다운로드 이미지 버튼 텍스트 중앙정렬

--- app/src/features/snippet/ui/DownLoadSnippetToSVGButton.tsx
+++ app/src/features/snippet/ui/DownLoadSnippetToSVGButton.tsx
@@ -32,7 +32,7 @@ export const DownLoadSnippetToSVGButton = () => {
 
   return (
     <button
-      className="bg-slate-700 rounded-xl text-sm px-4 py-2  mb-2 transition-transform duration-300 ease-in-out hover:scale-110      "
+      className="bg-slate-700 rounded-xl text-sm px-4 py-2 flex flex-col justify-center mb-2 transition-transform duration-300 ease-in-out hover:scale-110      "
       onClick={handleDownload}
     >
       Download Image
