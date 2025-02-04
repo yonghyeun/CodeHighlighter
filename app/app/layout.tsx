75af67fce12092a50c54dc46a25ac12b8fc8c733
yonghyeun <ttddcc119@naver.com>
2025-02-04 18:25:27 +0900

    faet[#13] : viewport Meta 데이터 생성

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -25,11 +25,12 @@ export const metadata: Metadata = {
   },
 };
 
-export const viewPort: Viewport = {
+export const viewport: Viewport = {
   width: "device-width",
-  initialScale: 1.25,
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
 };
-
 export default function RootLayout({
   children,
 }: Readonly<{
