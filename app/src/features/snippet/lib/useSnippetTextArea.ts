6ddb206f9b5c103a7ff2e9ad43536f7a7d2fb9be
yonghyeun <ttddcc119@naver.com>
2025-02-08 23:12:50 +0900

    feat[#20] : useSnippetTextArea에서 onKeydown으로 tab 시 텍스트 변경

--- app/src/features/snippet/lib/useSnippetTextArea.ts
+++ app/src/features/snippet/lib/useSnippetTextArea.ts
@@ -12,6 +12,26 @@ export const useSnippetTextArea = () => {
     dispatch(changeText(event.target.value));
   };
 
+  const onKeyDown = (event: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    if (event.key === "Tab") {
+      event.preventDefault();
+
+      if (!textArea.current) {
+        return;
+      }
+
+      const { newTextAreaValue, newCursorPosition } = insertTabSpaces(
+        textArea.current,
+        2
+      );
+
+      dispatch(changeText(newTextAreaValue));
+
+      textArea.current.value = newTextAreaValue;
+      textArea.current.setSelectionRange(newCursorPosition, newCursorPosition);
+    }
+  };
+
   // textArea의 높이를 자동으로 조절하는 효과를 위한 useEffect
   useEffect(() => {
     if (!textArea.current) {
@@ -25,5 +45,24 @@ export const useSnippetTextArea = () => {
     ref: textArea,
     defaultValue: text,
     onChange,
+    onKeyDown,
+  };
+};
+
+const insertTabSpaces = (
+  { selectionStart, selectionEnd, value }: HTMLTextAreaElement,
+  tabSize: number
+) => {
+  const beforeSelection = value.slice(0, selectionStart);
+  const afterSelection = value.slice(selectionEnd);
+  const newTextAreaValue = `${beforeSelection}${" ".repeat(
+    tabSize
+  )}${afterSelection}`;
+
+  const newCursorPosition = selectionStart + tabSize;
+
+  return {
+    newTextAreaValue,
+    newCursorPosition,
   };
 };
