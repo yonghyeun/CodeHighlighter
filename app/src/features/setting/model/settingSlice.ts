f696f5a3bb1593f926cc90e32e0c40468353088e
yonghyeun <ttddcc119@naver.com>
2025-02-09 18:13:27 +0900

    feat[#29] : settingSlice 에 title 타입 추가

--- app/src/features/setting/model/settingSlice.ts
+++ app/src/features/setting/model/settingSlice.ts
@@ -11,6 +11,7 @@ export type SettingState = {
   thirdUnderLineColor: string;
 
   showLineNumbers: string;
+  title: string;
 
   theme: string;
 };
@@ -30,6 +31,7 @@ const initialState = {
   secondUnderLineColor: "#644444",
   thirdUnderLineColor: "#646682",
   theme: "dracula",
+  title: "",
 } satisfies SettingState as SettingState;
 
 export const persistStoreSettingKeys: (keyof SettingState)[] = [
